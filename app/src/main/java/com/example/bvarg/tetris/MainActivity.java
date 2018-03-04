b63ad83740c19ffa882c049a9b0b450a95cce9b8
Bryan <bryanvargasf2110@github.com>
2018-03-04 00:09:54 -0600

    'cuadro'

--- app/src/main/java/com/example/bvarg/tetris/MainActivity.java
+++ app/src/main/java/com/example/bvarg/tetris/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bvarg.tetris;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GridLayout grid = (GridLayout) findViewById(R.id.gridlayouth);
-
+        int y = 0;
+        GridLayout grid = (GridLayout) findViewById(R.id.layoutTablero);
+        for (int i = 0; i < grid.getChildCount(); i++){
+            ImageView t = (ImageView) grid.getChildAt(i);
+            t.setImageResource(R.drawable.gris2);
+        }
     }
 }
